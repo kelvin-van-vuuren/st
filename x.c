6b88e5dcdec8be391c7e2e775985524b904c0f9a
Kelvin van Vuuren <kelvin@kelvinvv.xyz>
2022-11-28 09:50:08 +0000

    Patch: right click paste

--- x.c
+++ x.c
@@ -718,7 +718,9 @@ brelease(XEvent *e)
 
 	if (mouseaction(e, 1))
 		return;
-	if (btn == Button1)
+	if (btn == Button3)
+        selpaste(NULL);
+    else if (btn == Button1)
 		mousesel(e, 1);
 }
 
